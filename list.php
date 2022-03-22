2cd96f0667a0de9d09a2661262b204b89f48fda7
rudhrovai <102036685+rudhrovai@users.noreply.github.com>
2022-03-22 23:57:13 +0600

    W3codevs list practice

---
+++ list.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>list type practice</title>
+</head>
+<body>
+    <h1><marquee behavior="alternate"  direction="right">W3codevs Practice Work</marquee></h1>
+    <div class="1"> 
+        <h3>here are three list types in HTML:</h3>
+        <ul>
+            <li>unordered list — used to group a set of related items in no particular order.</li>
+            <li>ordered list — used to group a set of related items in a specific order.</li>
+            <li>description list — used to display name/value pairs such as terms and definitions.</li>  
+        </ul>
+    </div>
+    <div>
+        <h3>How do I show a list in HTML?</h3>
+        <ol>
+            <li>Use the HTML < ul> element to define an unordered list.</li>
+            <li>Use the CSS list-style-type property to define the list item marker</li>
+            <li>Use the HTML < li> element to define a list item.</li>
+            <li>Lists can be nested.</li>
+            <li>List items can contain other HTML elements.</li>
+            <li>Use the CSS property float:left to display a list horizontally.</li>
+            
+        </ol>
+    </div>
+
+    <div class="3">
+        <h3>What types of lists are there?</h3>
+        <ul type="circle">
+            <li>Bucket list. Such as "100 things to do before you die".</li>
+            <li>TODO list. Such as "Weekend tasks to complete". </li>
+            <li>Best-of list. Such as "Top 10 movies of all time"</li>
+            <li>Inventory list. Such as "Items for sale"</li>
+            <li>Brainstorming list. Such as this list.</li>
+        </ul>
+    </div>
+    <div class="4">
+        <h3>What kind of list should I make?</h3>
+        <strong>Make your lists something you can go back to when you need to de-stress or need a good laugh!</strong>
+        <ul type="disc">
+            <li>| Things That Make You Happy.</li>
+            <li>| Things You Are Good At.</li>
+            <li>| Things You Want to Learn.</li>
+            <li>| Things You Are Grateful For. </li>
+            <li>| Things You Don't Do Enough.</li>
+            <li>| Articles You Want to Read.</li>
+        </ul>
+    </div>
+    <div class="5">
+        <h3> How create list in HTML explain with example?</h3>
+        <ul type="square">
+            <li> < ul> − An unordered list. This will list items using plain bullets.</li>
+            <li> < ol> − An ordered list. This will use different schemes of numbers to list your items.</li>
+            <li> < dl> − A definition list. This arranges your items in the same way as they are arranged in a dictionary. </li>
+           
+        </ul>
+    </div>
+    <div class="unique">
+        <dl>
+            <dt>
+                <Strong>Don't try It</Strong>
+            </dt>
+            <dd>
+                <table border>
+                    <tr>
+                        <td>Name</td>
+                        <td>Roll</td>
+                        <td>Email</td>
+                    </tr>
+                    <tr>
+                        <td>Nazebul</td>
+                        <td>111111</td>
+                        <td>rudhro@onetaka.com</td>
+                    </tr>
+                </table>
+            </dd>
+        </dl>
+    </div>
+
+    <div class="unique1">
+        <h2 ALIGN=CENTER>We Love W3codevs </h2>
+        <code>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi, obcaecati. Facilis, perferendis esse voluptates ipsum, iste itaque consectetur ad saepe id repudiandae non voluptatum soluta similique assumenda doloremque doloribus, exercitationem in nisi vero voluptate! Placeat a quo,<br CLEAR=RIGHT>
+         vitae excepturi perferendis vel expedita consectetur et, minima ut totam pariatur autem ea! In at corporis, porro ea tempora iusto cum molestiae magnam.</code>
+    </div>
+
+    <div class="unique2">
+        <center>Hi There! Let's talk about stuff!</center>
+    </div>
+
+    <div class="hr">
+        <hr width="40%" align=left>
+        <hr width="40%" align=center>
+        <hr width="40%" align=right>
+    </div> <br> <br>
+    <div class="hr">
+        <hr width="25%">
+        <hr width="50%">
+        <hr  width="75%">
+        <hr width="100%">
+    </div>
+    
+</body>
+</html>
